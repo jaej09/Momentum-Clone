2a4f901ec4f9fe45cef7d63a13a7aaa4351ed2d8
Jae Jin <jaej09@outlook.com>
2020-08-15 14:48:14 +0900

    Display current time

---
+++ js/clock.js
@@ -0,0 +1,31 @@
+var Clock = (function() {
+	var $timeContainer, $time;
+
+	function getTime() {
+		var date, hours, minutes;
+
+		date = new Date();
+		hours = date.getHours() % 12;
+		minutes = date.getMinutes();
+
+		hours = hours ? hours : 12; // the hour '0' should be '12'
+		minutes = minutes < 10 ? '0' + minutes : minutes;
+
+		printTime(hours, minutes);
+	}
+
+	function printTime(hours, minutes) {
+		$time.innerText = hours + ':' + minutes;
+	}
+
+	function init() {
+		$timeContainer = document.querySelector('.page-main__time');
+		$time = $timeContainer.querySelector('.text');
+
+		setInterval(getTime, 1000);
+	}
+
+	return { init };
+})();
+
+Clock.init();
